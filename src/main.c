1c624cfceea4a34198f28aa9426153411f27f52a
Jyun-NengJi <jyunnengji@gmail.com>
2018-05-29 16:53:31 +0800

    asm file name

--- src/main.c
+++ src/main.c
@@ -3,21 +3,33 @@
 #include "scanner.h"
 
 FILE *source;
+FILE *code;
+char *codefile;
 int line_no = 1;
 
 int main(int argc, const char *argv[]) {
   char c;
+  int len;
 
   if (argc != 2) {
     fprintf(stderr, "No source file.\n");
     exit(1);
   }
+
+  // create asm file
+  len = strcspn(argv[1], ".");
+  codefile = (char *)malloc(sizeof(char));
+  strncpy(codefile, argv[1], len);
+  strcat(codefile, ".asm");
+  code = fopen(codefile, "w");
+
+  // read source file
   source = fopen(argv[1], "r");
   if (source == NULL) {
     fprintf(stderr, "File %s not found\n", argv[1]);
     exit(1);
   }
-  fprintf(stdout, "\n File %s read\n", argv[1]);
+  fprintf(stdout, "\n Compile start, please wait.... \n");
 
   parser();
 
