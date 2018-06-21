f143ddafd91213dc8d060ff121c292743eaf055d
Jyun-NengJi <jyunnengji@gmail.com>
2018-06-21 17:11:15 +0800

    symbol, operand, and operator structure

--- inc/symtab.h
+++ inc/symtab.h
@@ -2,51 +2,57 @@
 #define _SYMTAB_H_
 #include "global.h"
 
-typedef struct Operand {
-  char *op_name;
-  int n;
-  TokenType op_type;
-  struct Operand *next;
-} Operand;
-
-typedef struct Operator {
-  TokenType opr_type;
-  struct Operator *next;
-} Operator;
-
-Operand *operand;
-Operator *operator;
-
+// declaration variable struct node
 typedef struct symbol {
-  char *dcl_name;
+  char dcl_name[MAXTOKENLEN];
   int n;
   union dcl_val {
     int num;
-    char *str;
+    char str[MAXTOKENLEN];
   } dcl_val;
   TokenType dcl_type;
   struct symbol *next;
 } Symbol;
 
-Symbol *sym;
+// expression operand struct node
+typedef struct Operand {
+  char op_name[MAXTOKENLEN];
+  int n;
+  TokenType op_type;
+  struct Operand *next;
+} Operand;
 
-void st_init();  // symbol table initialization
+// expression operator struct node
+typedef struct Operator {
+  TokenType opr_type;
+  struct Operator *next;
+} Operator;
+
+/* Symbol table initialization. */
+void st_init();
+/* Operand and operator stack initialization. */
 void op_init();
-void operand_traversal();
-/*
- * There are 4 declaration data type, STRTYPE, CHARTYPE, NUM, and BOOLEAN.
+/* There are 4 declaration data type, STRTYPE, CHARTYPE, NUM, and BOOLEAN.
  * Constant declaration data type is according to its assign value. If assigned
  * data is TRUE or FALSE, constant declaration data type will be BOOLEAN. If
  * assigned data is another declaration data, using st_lookup function to find
  * the data and get its data type.
  */
-void st_insert(char *dcl_name, char *dcl_char,
-               TokenType dcl_type);  // add new data
-Symbol *st_lookup(char *srch_name);  // find data in the symbol table
-void st_print();  // print data that store in the symbol table
+void st_insert(char *dcl_name, int n, char *dcl_char,
+               TokenType dcl_type);
+/* Pop symbol structure node from symbol table. */
+Symbol *st_pop();
+/* Search if the identifier is declared. */
+Symbol *st_lookup(char *srch_name, int n);
+/* Print symbol table. */
+void st_print();
+/* Add operand symbol to operand stack. */
 void push_operand(Symbol *op);
+/* Add operator type to operator stack. */
 void push_operator(TokenType opr_type);
+/* Pop operand out from the operand stack. */
 Operand *pop_operand();
+/* Pop operator out from the operator stack. */
 Operator *pop_operator();
 
 #endif
